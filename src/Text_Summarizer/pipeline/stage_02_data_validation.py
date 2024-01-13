29ddc891a0dbcfa28b9764076a362d2abd3002b7
MaazSayyed05 <maazsayyed05@gmail.com>
2024-01-13 15:19:51 +0530

    data validation notebook added

---
+++ src/Text_Summarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,17 @@
+import os
+from Text_Summarizer.config import ConfigurationManager
+from Text_Summarizer.components.data_validation import DataValidation
+
+from Text_Summarizer.logging import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+
